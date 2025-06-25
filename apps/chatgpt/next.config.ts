e788e3c16cfb370cc88c30402c2b9232ce4283eb
anuragdaksh7 <anuragdaksh77777@gmail.com>
2025-06-25 06:54:42 +0000

    build fix

--- apps/chatgpt/next.config.ts
+++ apps/chatgpt/next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -12,6 +16,15 @@ const nextConfig: NextConfig = {
         search: '',
       },
     ],
+  },
+  env: {
+  	GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+	DATABASE_URL: process.env.DATABASE_URL,
+	NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+	CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+	NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+	NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL,
+	NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL,
   }
 };
 
